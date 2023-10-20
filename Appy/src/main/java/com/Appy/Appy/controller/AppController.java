81fdadde2c475ea1fe0a74e371041007420d222b
Austin Snyder <132637314+AustinSnyd3r@users.noreply.github.com>
2023-10-20 13:19:48 -0700

    1:18PM 10/20/2023.
    mappings are working. postman is able to send post requests, but havent gotten database table to be created from inside spring

--- Appy/src/main/java/com/Appy/Appy/controller/AppController.java
+++ Appy/src/main/java/com/Appy/Appy/controller/AppController.java
@@ -1,15 +1,31 @@
 package com.Appy.Appy.controller;
-
+import com.Appy.Appy.model.App;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.Appy.Appy.repository.AppRepository;
+import com.Appy.Appy.services.AppService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/app")
+@CrossOrigin
 public class AppController {
     
     @Autowired
-    private AppRepository appRepository;
+    private AppService appService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody App app){
+        appService.saveApp(app);
+        return "New application was added.";
+    }
+
+    @GetMapping("/getAll")
+    public List<App> list(){
+        return appService.getAllApps();
+    }
 }
--- Appy/src/main/java/com/Appy/Appy/services/AppService.java
+++ Appy/src/main/java/com/Appy/Appy/services/AppService.java
@@ -1,5 +1,10 @@
 package com.Appy.Appy.services;
+import com.Appy.Appy.model.App;
+import java.util.List;
 
 public interface AppService {
-    
+
+    //abstract class
+    public App saveApp(App app);
+    public List<App> getAllApps();
 }
